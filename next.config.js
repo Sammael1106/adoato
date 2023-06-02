8e35897ea533f2312a038d8116e1bd08b8714fa2
Darya <sammael1106@yandex.ru>
2023-06-02 10:04:27 +0200

    three.js setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ['@acme/ui', 'lodash-es'],
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+        test: /\.hdr$/i,
+        loader: 'url-loader'
+    });
+
+    return config;
+  },
+  staticPageGenerationTimeout: 120
+}
 
 module.exports = nextConfig
